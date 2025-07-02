2aebb5b31c72ab597192762300dd00979e2f83c5
David Fritsch <fritsch.david@gmail.com>
2025-07-02 21:01:25 +0200

    david_update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='his is an example project for an excercise for the Constructor Academy Bootcamp. It is about predicting prizes of the california-housing-dataset (https://drive.google.com/file/d/1ZdY3c2n5EewMgBLSI0P3U91PSRoq40Mk/view?usp=sharing)',
+    author='David Fritsch',
+    license='MIT',
+)
